1be570251bbf7c3c05b31fc7ccf0e9c25b4b8a36
Anyaele <kirchoff200@gmail.com>
2024-11-07 13:30:10 +0100

    created the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home"
+import About from "./pages/about"
+import Profile from "./pages/profile"
+import NotFound from "./pages/not-found"
+import Signin from "./pages/_auth/sign-in"
+import Signup from "./pages/_auth/sign-up"
+import Header from "./common/header"
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      App
-    </div>
+    <>
+  <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/sign-in" element={<Signin />} />
+    <Route path="/sign-up" element={<Signup />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+    </>
   )
 }
 
--- src/pages/_auth/sign-in.jsx
+++ src/pages/_auth/sign-in.jsx
@@ -2,7 +2,7 @@
 const Signin = () => {
   return (
     <div>
-      
+      signin
     </div>
   )
 }
