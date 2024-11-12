db218a783e39843a2f847df0f2e9a4f205beafd0
Anyaele <kirchoff200@gmail.com>
2024-11-12 17:48:09 +0100

    add database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+    console.log("Connected to MongoDB")
+}).catch((err) => {
+    console.log(err);
+    
+})
 
 const app = express(); 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
     
+})
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Api is working"
+    })
 })
