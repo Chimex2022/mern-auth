c5743d2af0c9d414c7118978c2360411c4c7458d
Anyaele <kirchoff200@gmail.com>
2024-11-12 18:29:45 +0100

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRoutes from "./routes/user-routes.js"
 dotenv.config();
 
 
@@ -20,8 +21,4 @@ app.listen(3000, () => {
 })
 
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Api is working"
-    })
-})
+app.use("/api/user", userRoutes)
